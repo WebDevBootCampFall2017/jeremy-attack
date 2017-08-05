89d9568ac63cd058a4c502270b283fc6c1978c0a
Namingwaysway <namingwaysway@gmail.com>
2017-08-04 22:34:39 -0400

    Update Program.cs

--- Attacks/Program.cs
+++ Attacks/Program.cs
@@ -1,12 +1,18 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 
 namespace Attacks
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            Dictionary<String, int> attacks = new Dictionary<string, int>();
+            attacks.Add("Bash", 12);
+            attacks.Add("Slice", 12);
+            attacks.Add("Stab", 12);
+
+			Console.WriteLine("Hello World!");
+		}
+	}
 }
